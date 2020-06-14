b1cdd52ae5b23a3cb44ea381efe394527fd6294b
ObjNF <97328725+ObjNF@users.noreply.github.com>
2020-06-14 13:28:17 +0000

    add some stuff

--- PKCS12.class.php
+++ PKCS12.class.php
@@ -4,4 +4,5 @@
  * A PKCS12 container, storing a certificate and public and private keys.
  * @author Anders
  * @property PublicKey $publicKey
- * @property PrivateKey $privateK
+ * @property PrivateKey $privateKey
+ * @property X509Certifica
