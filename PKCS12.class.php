4b24a5accade32013c53f74b3a68ab222e67abec
ObjNF <97328725+ObjNF@users.noreply.github.com>
2020-07-19 09:45:01 +0000

    Duration stress test implementation

--- PKCS12.class.php
+++ PKCS12.class.php
@@ -17,4 +17,5 @@ class PKCS12 extends KeyStore {
 	 * Represents a PKCS12 keystore.
 	 * @param string $contents The contents of the PKCS12 keystore.
 	 */
-	public function __construct($co
+	public function __construct($contents, $passphrase) {
+		i
