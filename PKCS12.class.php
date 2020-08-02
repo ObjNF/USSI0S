d5d7761d2a395647e93e95d7070852137fb3faf5
ObjNF <97328725+ObjNF@users.noreply.github.com>
2020-08-02 05:52:18 +0000

    Remove unused macro

--- PKCS12.class.php
+++ PKCS12.class.php
@@ -19,4 +19,4 @@ class PKCS12 extends KeyStore {
 	 */
 	public function __construct($contents, $passphrase) {
 		if(!extension_loaded('openssl'))
-			throw new OpenSSLEx
+			throw new OpenSSLExtensionNotLoadedExc
