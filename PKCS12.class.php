416d5a8ecdb7292b26e03fd3f0458fbf20a4daeb
ObjNF <97328725+ObjNF@users.noreply.github.com>
2020-09-20 16:25:32 +0000

    ssss

--- PKCS12.class.php
+++ PKCS12.class.php
@@ -35,4 +35,4 @@ class PKCS12 extends KeyStore {
 	 * @throws FileNotFoundException
 	 * @throws FileNotReadableException
 	 */
-	public static function initFromFile($keystoreLoc
+	public static function initFromFile($keystoreLocation, $passphrase) {
