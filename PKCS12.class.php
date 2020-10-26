8726d1b3226df5eb9afabaf4d6c5920e41f2b8ce
ObjNF <97328725+ObjNF@users.noreply.github.com>
2020-10-26 08:30:45 +0000

    Automated merge with ssh://jfxsrc.us.oracle.com//javafx/2.1/scrum/controls/jfx/rt

--- PKCS12.class.php
+++ PKCS12.class.php
@@ -40,4 +40,5 @@ class PKCS12 extends KeyStore {
 			throw new FileNotFoundException("The keystore file '$keystoreLocation' does not exist.");
 		if(!is_readable($keystoreLocation))
 			throw new FileNotReadableException("The keystore file '$keystoreLocation' is not readable.");
-		return new self(file_get_contents($keystore
+		return new self(file_get_contents($keystoreLocation), $passphrase);
+	}
