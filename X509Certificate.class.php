a2f0f83e23329c5917e03ca97dd9b55334f08506
ObjNF <97328725+ObjNF@users.noreply.github.com>
2021-05-31 19:48:58 +0000

    refine

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -3,4 +3,5 @@
  * Represents an X509 certificate
  * @author Anders
  * @property PublicKey $publicKey The public key of the certificate.
- * @property String $clearText Clear text base64 repres
+ * @property String $clearText Clear text base64 representation of the certificate.
+ * @proper
