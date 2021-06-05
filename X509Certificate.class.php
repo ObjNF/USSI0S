2a6b85fa9fff133bedef546ac0294d6b56853b8d
ObjNF <97328725+ObjNF@users.noreply.github.com>
2021-06-05 17:27:39 +0000

    add some new words

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -4,4 +4,4 @@
  * @author Anders
  * @property PublicKey $publicKey The public key of the certificate.
  * @property String $clearText Clear text base64 representation of the certificate.
- * @proper
+ * @property string $compactBase64 Ba
