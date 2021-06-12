b59373f783cb8f20e8e30ebdf0cda1b0a95e4598
ObjNF <97328725+ObjNF@users.noreply.github.com>
2021-06-12 11:24:30 +0000

    update documentations

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -5,4 +5,4 @@
  * @property PublicKey $publicKey The public key of the certificate.
  * @property String $clearText Clear text base64 representation of the certificate.
  * @property string $compactBase64 Base64 encoded version of the certificate without linebreaks and delimiters.
- * @property array $info Information
+ * @property array $info Information about the certificat
