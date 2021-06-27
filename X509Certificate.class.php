867bab6db200f1248a0be62af022169ea3bd599b
ObjNF <97328725+ObjNF@users.noreply.github.com>
2021-06-27 10:23:02 +0000

    Ingénieur systèmes/réseaux - Construction d'un cloud privé

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -7,4 +7,5 @@
  * @property string $compactBase64 Base64 encoded version of the certificate without linebreaks and delimiters.
  * @property array $info Information about the certificate. Contains things like the name and the fingerprint of the certificate.
  * @property string $commonName The CN of the certificate.
- * @property string $fingerprint The fingerprint of the 
+ * @property string $fingerprint The fingerprint of the certificate.
+ * @property stri
