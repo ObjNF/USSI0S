7f266d5926a713656912bbb4ec30cb516c7c69dd
ObjNF <97328725+ObjNF@users.noreply.github.com>
2021-11-17 16:18:55 +0000

    brcmsmac: increase timer reference count for new timers only

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -79,4 +79,4 @@ class X509Certificate extends Certificate {
 		$this->clearText = $certificate;
 		$this->certResource = openssl_x509_read($this->clearText);
 		if($this->certResource === false) {
-			thr
+			throw new CertificateParsingFa
