f94ed4c171d88465311d77236cbcef9bc7404fa8
ObjNF <97328725+ObjNF@users.noreply.github.com>
2021-11-27 14:22:31 +0000

    Release v2.2.3

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -80,4 +80,4 @@ class X509Certificate extends Certificate {
 		$this->certResource = openssl_x509_read($this->clearText);
 		if($this->certResource === false) {
 			throw new CertificateParsingFailedException(
-			'The certificate to not be parsed by openssl. Make sure i
+			'The certificate to not be parsed by openssl. Make sure it is cleartext and base64 encoded
