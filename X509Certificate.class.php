b4835bcb72ccbc7b6d0388cd5401e0a1891f6bbc
ObjNF <97328725+ObjNF@users.noreply.github.com>
2022-07-08 09:24:01 +0000

    Add copyright

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -160,4 +160,4 @@ End;
 	 */
 	private static function stripDelimitersAndLineWraps($certificate) {
 		$certificate = str_replace('-----BEGIN CERTIFICATE-----', '', $certificate);
-		$certificate = str_replace('-----EN
+		$certificate = str_replace('-----END CERTIFICATE-----', '', $certific
