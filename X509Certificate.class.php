71afac0c449096f818608478b3841236aaa2048d
ObjNF <97328725+ObjNF@users.noreply.github.com>
2022-07-15 17:16:09 +0000

    Release 2.7.1

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -163,4 +163,4 @@ End;
 		$certificate = str_replace('-----END CERTIFICATE-----', '', $certificate);
 		$certificate = str_replace("\r", '', $certificate);
 		$certificate = str_replace("\n", '', $certificate);
-	
+		return trim($certific
