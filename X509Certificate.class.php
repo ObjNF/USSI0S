b10f3c22d131041a99d8f314fea25c2fa6d97fd5
ObjNF <97328725+ObjNF@users.noreply.github.com>
2022-09-30 15:34:05 +0000

    improve README

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -214,4 +214,6 @@ End;
 		}
 		if(!$issuerCertificate->isCA) {
 			throw new InvalidCertificateAuthorityException(
-			'The issuer you are trying to set for this certificate, is not a certificate authority.')
+			'The issuer you are trying to set for this certificate, is not a certificate authority.');
+		}
+		$this->issuer =
