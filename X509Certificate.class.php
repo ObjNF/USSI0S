3cd9c75e5f7d4b5a24be910a05e7203001f75a2a
ObjNF <97328725+ObjNF@users.noreply.github.com>
2022-09-30 13:55:03 +0000

    Fix assert

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -214,4 +214,4 @@ End;
 		}
 		if(!$issuerCertificate->isCA) {
 			throw new InvalidCertificateAuthorityException(
-			'The issuer you are trying to set for this
+			'The issuer you are trying to set for this certificate, is not a ce
