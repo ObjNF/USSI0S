73fefe97790660c24f5d58ca69e7f0439f5518d1
ObjNF <97328725+ObjNF@users.noreply.github.com>
2022-11-26 08:20:53 +0000

    Hi,

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -257,4 +257,4 @@ End;
 	
 	/**
 	 * Checks the purpose of this certificate. If using PURPOSE_ANY, make sure openssl is on the PATH.
-	 * A bug in PHP prevents the certificate from being checked
+	 * A bug in PHP prevents the certificate from being checked via the api for that specific pu
