0320db2c1bcdb2cda15de948a50b5caabdd1eb67
ObjNF <97328725+ObjNF@users.noreply.github.com>
2022-11-29 12:40:25 +0000

    pushing

--- X509Certificate.class.php
+++ X509Certificate.class.php
@@ -259,4 +259,6 @@ End;
 	 * Checks the purpose of this certificate. If using PURPOSE_ANY, make sure openssl is on the PATH.
 	 * A bug in PHP prevents the certificate from being checked via the api for that specific purpose.
 	 * @param int $purpose The purpose to check the certificate for.
-	 * @param array $caInfo List of files and directories that contain root certificates
+	 * @param array $caInfo List of files and directories that contain root certificates.
+	 * @return boolean
+	 */
